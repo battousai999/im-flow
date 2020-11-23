37924b8a7081c8edb5e4a8e75201d884849188f8
Kurtis Jones <battousai999@gmail.com>
2020-11-23 01:28:56 -0700

    Add log entry filtering

--- im-flow-fsharp/Entry.fs
+++ im-flow-fsharp/Entry.fs
@@ -8,9 +8,41 @@ let payloadDetailsRegex = Regex(@"^<([\w\d.]+)>\smessage\sdetails:", RegexOption
 let sentToGenesysRegex = Regex(@"Sending\s<([\w\d.]+)>\smessage\sto\s<TServer>", RegexOptions.IgnoreCase)
 let sentToFubuRegex = Regex(@"Sending\s<([\w\d.]+)>\smessage\sto\s<CoreBus>", RegexOptions.IgnoreCase)
 let sentToSscRegex = Regex(@"Sending\s<([\w\d.]+)>\smessage\sto\s<SSC>", RegexOptions.IgnoreCase)
+let sentToTimServiceRegex = Regex(@"Sending\s<([\w\d/]+)>\srequest\sto\s<TIM\sservice>", RegexOptions.IgnoreCase)
 let receivedFromGenesysRegex = Regex(@"^Received\sGenesys\smessage:\s+([^\s]+)", RegexOptions.IgnoreCase)
 let receivedFromFubuRegex = Regex(@"^Received\s<([\w\d.]+)>\smessage\sfrom\s<lq\.tcp://[^>]+>", RegexOptions.IgnoreCase)
 let receivedFromSscRegex = Regex(@"^Received\s<([\w\d.]+)>\smessage\sfrom\s<SSC>", RegexOptions.IgnoreCase)
+
+let specialInfoRegexes = [
+    Regex(@"^(\(for\sWTWCallId\s=\s\d+,\sConnID\s=\s[\da-f]+\))$", RegexOptions.IgnoreCase)
+    Regex(@"^(\(Initialize\soutbound\scall\sfor\sWTWCallId\s=\s\d+,\sConnID\s=\s[\da-f]+\))$", RegexOptions.IgnoreCase)
+    Regex(@"^(Unregistering\sCall\s\([\da-f]+\))\sas\slistener\sfor\sAcceptOfferMessage\.\.\.$", RegexOptions.IgnoreCase)
+    Regex(@"^(Adding\sparticipant\s'[^']*',\slist\sis\snow\s\[[^\]]+])", RegexOptions.IgnoreCase)
+    Regex(@"^(Removing\sparticipant\s'[^']*',\slist\sis\snow\s\[[^\]]*\])", RegexOptions.IgnoreCase)
+    Regex(@"^(Setting\sparticipants\sfrom\sattached\sdata,\slist\sis\snow\s\[[^\]]+])", RegexOptions.IgnoreCase)
+    Regex(@"^(Created\sconsultation\scall\sobject\s\([\da-f]+\))$", RegexOptions.IgnoreCase)
+    Regex(@"^(Waiting\sfor\sEventAttachedDataChanged\shaving\sdifferent\sRTargetAgentSelected,\sfound\schange\sfrom\s'.*'\sto\s'.*'.)$", RegexOptions.IgnoreCase)
+    Regex(@"^(Interceptor\sversion:\s+.*)$", RegexOptions.IgnoreCase)
+    Regex(@"^(Registering\sGenesys\saddress\s\(.+\))\.\.\.$", RegexOptions.IgnoreCase)
+    Regex(@"^(Retrieving\suser-specific\ssettings\s\(for\s'[^']*'\))\.\.\.", RegexOptions.IgnoreCase)
+    Regex(@"^(Using\sLocal\sURI\s\(lq.tcp://[^/]*/interceptor\))", RegexOptions.IgnoreCase)
+    Regex(@"^(Channel\s\w+\son\s\w+\sendpoint\s\([^)]*\))", RegexOptions.IgnoreCase)
+    Regex(@"^(Set\senvironment\sspecific\sskill:\ss\.Env\.[\w\d]+)", RegexOptions.IgnoreCase)
+    Regex(@"^(Setting\sAgentSipUri\sto:\s.+)$", RegexOptions.IgnoreCase)
+    Regex(@"^(Setting\sAgentEmployeeId\sto:\s\d+)", RegexOptions.IgnoreCase)
+    Regex(@"^(Unfinished\scall\sfile\sfound\s-\sattempting\sto\srecreate\scall\sobjects.)", RegexOptions.IgnoreCase)
+    Regex(@"^(Genesys\sreports\sno\sunfinished\scalls)", RegexOptions.IgnoreCase)
+    Regex(@"^(Unfinished\scall\s[\da-f]+\srecreated)", RegexOptions.IgnoreCase)
+    Regex(@"^(Swapping\sheld\scalls\sActive:\s[\da-f]+\sHeld:\s[\da-f]+)", RegexOptions.IgnoreCase)
+    Regex(@"^(Deferring\sremoval\sof\sparticipant\s\(DN=[^)]+\)\suntil\scall\sinvitation\saccepted)", RegexOptions.IgnoreCase)
+    Regex(@"^(Connection\sID\schanged\sfrom\s[\da-f]+\sto\s[\da-f]+)", RegexOptions.IgnoreCase)
+    Regex(@"^(Assign\snew\sCallEndpointNumber\s\([^)]+\)\sfrom\sparticipant\sserialization\sfor\s[^/]*/.*)$", RegexOptions.IgnoreCase)
+    Regex(@"^(>>>.*)$")
+]
+
+let ignoredInfoRegexes = [
+    Regex(@"Passing through message to", RegexOptions.IgnoreCase)
+]
 
 type Entry = {
     Filename : string
@@ -24,17 +56,36 @@ type Entry = {
 }
 
 
-let isSentToGenesys (entry : Entry) = sentToGenesysRegex.IsMatch(entry.LogMessage)
-let isSentToFubu (entry : Entry) = sentToFubuRegex.IsMatch(entry.LogMessage)
-let isSentToSsc (entry : Entry) = sentToSscRegex.IsMatch(entry.LogMessage)
+let isSentToGenesys entry = sentToGenesysRegex.IsMatch(entry.LogMessage)
+let isSentToFubu entry = sentToFubuRegex.IsMatch(entry.LogMessage)
+let isSentToSsc entry = sentToSscRegex.IsMatch(entry.LogMessage)
+let isSentToTimService entry = sentToTimServiceRegex.IsMatch(entry.LogMessage)
 
-let isReceivedFromGenesys (entry : Entry) = receivedFromGenesysRegex.IsMatch(entry.LogMessage)
-let isReceivedFromFubu (entry : Entry) = receivedFromFubuRegex.IsMatch(entry.LogMessage)
-let isReceivedFromSsc (entry : Entry) = receivedFromSscRegex.IsMatch(entry.LogMessage)
+let isReceivedFromGenesys entry = receivedFromGenesysRegex.IsMatch(entry.LogMessage)
+let isReceivedFromFubu entry = receivedFromFubuRegex.IsMatch(entry.LogMessage)
+let isReceivedFromSsc entry = receivedFromSscRegex.IsMatch(entry.LogMessage)
+
+let isSentMessage entry = (isSentToGenesys entry) || (isSentToFubu entry) || (isSentToSsc entry) || (isSentToTimService entry)
+let isReceivedMessage entry = (isReceivedFromGenesys entry) || (isReceivedFromFubu entry) || (isReceivedFromSsc entry)
+let isMessage entry = (isSentMessage entry) || (isReceivedMessage entry)
 
 let isGenesysMessage entry = (isSentToGenesys entry) || (isReceivedFromGenesys entry)
 let isFubuMessage entry = (isSentToFubu entry) || (isReceivedFromFubu entry)
 let isSscMessage entry = (isSentToSsc entry) || (isReceivedFromSsc entry)
+
+let isSpecialInfo entry = specialInfoRegexes |> List.exists (fun (x : Regex) -> x.IsMatch(entry.LogMessage))
+let isIgnoredInfo entry = ignoredInfoRegexes |> List.exists (fun (x : Regex) -> x.IsMatch(entry.LogMessage))
+
+let isNonMessageInfo entry = 
+    StringComparer.OrdinalIgnoreCase.Equals(entry.LogLevel, "info") && 
+    (not << isMessage <| entry) && 
+    (not << isIgnoredInfo <| entry)
+
+let isIgnoredWarning entry = StringComparer.OrdinalIgnoreCase.Equals(entry.LogMessage, "setting name 'Interceptor' is invalid")
+
+let isError entry = StringComparer.OrdinalIgnoreCase.Equals(entry.LogLevel, "error")
+let isFatal entry = StringComparer.OrdinalIgnoreCase.Equals(entry.LogLevel, "fatal")
+let isWarning entry = StringComparer.OrdinalIgnoreCase.Equals(entry.LogLevel, "warn") && (not << isIgnoredWarning <| entry)
 
 
 let getGenesysMessage (entry : Entry) =
--- im-flow-fsharp/Program.fs
+++ im-flow-fsharp/Program.fs
@@ -5,6 +5,7 @@ open Utils
 open OutputWriter
 open im_flow
 open type Battousai.Utils.ConsoleUtils
+open System.Collections.Generic
 
 let app (parameters : Args) =
     let expandFiles filename =
@@ -21,9 +22,33 @@ let app (parameters : Args) =
 
     let rawEntries = EntryProcessing.readRawEntries allFilenames
     let entries = rawEntries |> EntryProcessing.parseEntries parameters.ParseLogDatesAsLocal
+
+    let isError = fun x -> (not parameters.IgnoreErrors) && ((Entry.isError x) || (Entry.isFatal x) || (Entry.isWarning x))
+    let isFullInfo = fun x -> parameters.ShowAllInfoMessages && (Entry.isNonMessageInfo x)
+
+    let entriesFilter = fun x ->
+        ((Entry.isMessage x) || (isError x) || (Entry.isSpecialInfo x) || (isFullInfo x)) &&
+        (parameters.IncludeHeartbeat || (not <| StringComparer.OrdinalIgnoreCase.Equals((Entry.getGenesysMessage x), "EventAddressInfo")))
+
+    let entriesComparer = fun (x : Entry.Entry) (y : Entry.Entry) ->
+        let dateComparer (x : Entry.Entry) (y : Entry.Entry) = Comparer<DateTimeOffset>.Default.Compare(x.LogDate, y.LogDate)
+        let secondaryComparer (x : Entry.Entry) (y : Entry.Entry) = 
+            let intComparer = Comparer<int>.Default.Compare
+
+            if StringComparer.OrdinalIgnoreCase.Equals(x.Filename, y.Filename)
+            then intComparer(x.LineNumber, y.LineNumber)
+            else -intComparer(x.LineNumber, y.LineNumber)
+
+        match (dateComparer x y) with
+        | value when value <> 0 -> value
+        | _ -> secondaryComparer x y
+
+    let filteredEntries = entries 
+                            |> List.filter entriesFilter
+                            |> List.sortWith entriesComparer
+
     use streamWriter = if shouldOutputToConsole then null else new StreamWriter(outputFilename, false)
     let outputWriter = if shouldOutputToConsole then buildConsoleWriter() else buildFileWriter(streamWriter)
-
 
     ()
 
